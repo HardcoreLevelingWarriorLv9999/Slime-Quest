1787949740f626baac41ce321feba442939bc44a
phongtt03 <quocthongmelinh@gmail.com>
2024-03-13 09:49:52 +0700

    PauseMenu

---
+++ Assets/pause menu/pauseMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject PauseMenu;
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Volum()
+    {
+        Debug.Log("tang giam volum");
+    }
+}
